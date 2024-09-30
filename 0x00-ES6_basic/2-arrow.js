553003ea19e2f1f4054ea8af7d02e73f7fa36e8b
khalidhub7 <louguikhalid7@gmail.com>
2024-09-30 14:36:43 -0700

    Arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = () => add((newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  });
+}
