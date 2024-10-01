103a9a8a7444278982310984896eb01156a2f571
khalidhub7 <louguikhalid7@gmail.com>
2024-09-30 18:25:05 -0700

    up

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,16 +1,9 @@
-import getBudgetObject from './7-getBudgetObject';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
   };
 
-  return fullBudget;
+  return budget;
 }
