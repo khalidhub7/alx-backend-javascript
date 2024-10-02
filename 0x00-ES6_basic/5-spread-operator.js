42b6651e1ce2d2732f227a1a27c8d8525200e324
khalidhub7 <louguikhalid7@gmail.com>
2024-10-01 18:04:05 -0700

    spread syntax

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,7 @@
 export default function concatArrays(array1, array2, string) {
-  const mylist = [...array1, ...array2, ...string];
-  return mylist;
+    let khalid_list = [];
+    khalid_list.push(...array1);
+    khalid_list.push(...array2);
+    khalid_list.push(...string);
+    return khalid_list;
 }
