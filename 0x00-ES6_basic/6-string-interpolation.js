1b39954e6945000e8a1d15c1345fbff5f0f04d30
khalidhub7 <louguikhalid7@gmail.com>
2024-10-01 18:17:41 -0700

    string in js

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,9 +5,10 @@ export default function getSanFranciscoDescription() {
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
-  const text = `As of ${year}, it was the seventh-highest income county in the United States\
-, with a per capita personal income of ${budget.income}. As of 2015, San Fr\
-ancisco proper had a GDP of ${budget.gdp}, and a GDP per capi\
-ta of ${budget.capita}.`;
-  return text;
+
+  return `As of ${year}, it was the seventh-highest i\
+ncome county in the United States, with a per capita pe\
+rsonal income of ${budget.income}. As of 2015, San Franci\
+sco proper had a GDP of ${budget.gdp}, and a GDP per capit\
+a of ${budget.capita}.`;
 }
