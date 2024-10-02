b28a584e9630e1983632c44e0b8abc90c98939ff
khalidhub7 <louguikhalid7@gmail.com>
2024-10-02 16:17:09 -0700

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,5 @@
-const getFullResponseFromAPI = (success) => {
+/* my original code
+ const getFullResponseFromAPI = (success) => {
   const mypromise = new Promise((resolve, reject) => {
     if (success) {
       resolve({
@@ -11,4 +12,16 @@ const getFullResponseFromAPI = (success) => {
   });
   return mypromise;
 };
-export default getFullResponseFromAPI;
+export default getFullResponseFromAPI; */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
