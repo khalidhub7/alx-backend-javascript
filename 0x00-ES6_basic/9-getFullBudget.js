db4128fa2c8c8f5d818c0565ab48e4094a552993
khalidhub7 <louguikhalid7@gmail.com>
2024-10-01 19:05:54 -0700

    ES6 fix

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,10 +4,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
+    getIncomeInDollars(this) {
       return `$${this.income}`;
     },
-    getIncomeInEuros() {
+    getIncomeInEuros(this) {
       return `${this.income} euros`;
     },
   };
