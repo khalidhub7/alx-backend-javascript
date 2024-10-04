8b3066c9f3dfaedf4e0d19e667c32eae0c4b58ee
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 14:22:59 -0700

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise.all(([uploadPhoto(), createUser()]))
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
