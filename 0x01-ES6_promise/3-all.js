519595db0e39b7feb8d6a7c74aff00e3d6cb347f
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 14:24:56 -0700

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,13 +3,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all(([uploadPhoto(), createUser()]))
-    .then((results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    })
-    .finally(() => {
-      console.log('done');
-    });
+    .then((results) => { console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); })
+    .finally(() => { console.log('done'); });
 }
