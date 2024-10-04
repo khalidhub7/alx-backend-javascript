f299d2ec871b98c8217503b26b43190745c79f94
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 15:15:44 -0700

    simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,9 @@
+#!/usr/bin/env node
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
+  return new Promise((resolve) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+  });
 }
