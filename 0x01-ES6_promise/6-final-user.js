7523a77939d61c2efe835f54c9575eccc0e96bfc
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 16:05:47 -0700

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .all([signUpUser(), uploadPhoto()])
