4755908c00618c2d93985778bb1ad99535c349f0
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:59:36 -0700

    try catch in js not python this time hhh

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw Error('cannot divide by 0');
-  } else {
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
     return numerator / denominator;
+  } catch (i) {
+    throw (i);
   }
 }
