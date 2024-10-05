e6ea6111363e12143d7c75bed92d78d9f00dee88
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:02:59 -0700

    handle using allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,22 +9,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       uploadPhoto(fileName),
     ])
     .then((data) => {
-      const khalid = [];
-      let i = 0;
-      while (i < data.length) {
-        if (data[i].status === 'fulfilled') {
-          khalid.push({
-            status: data[i].status,
-            value: data[i].value,
-          });
-        } else if (data[i].status === 'rejected') {
-          khalid.push({
-            status: data[i].status,
-            reason: data[i].reason,
-          });
-        }
-        i++;
-      }
-      return khalid;
+      return data.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
     });
 }
