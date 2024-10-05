b09c75a8fe61bebd5ce62c5942ee095586c5462c
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 15:20:45 -0700

    classRoom obj

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,6 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [19, 20, 34].map((size) => new ClassRoom(size));
+  const lisst = [19, 20, 34];
+  return lisst.map((i) => new ClassRoom(i));
 }
