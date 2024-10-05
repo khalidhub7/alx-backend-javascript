f0b9ac86b75f8e3360cbbd099a5530241c534068
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:09:55 -0700

    handle using allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    .then((data) => (
+      data.map((i) => ({
+        status: i.status,
+        value: i.status === 'fulfilled' ? i.value : String(i.reason),
       }))
     ));
 }
