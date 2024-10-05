9f64e83a36f02e90d7e79d77700deeec613d70de
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:00:55 -0700

    handle using allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,27 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([signUpUser(), uploadPhoto()])
-    .then((data) => data);
+    .allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ])
+    .then((data) => {
+      const khalid = [];
+      let i = 0;
+      while (i < data.length) {
+        if (data[i].status === 'fulfilled') {
+          khalid.push({
+            status: data[i].status,
+            value: data[i].value,
+          });
+        } else if (data[i].status === 'rejected') {
+          khalid.push({
+            status: data[i].status,
+            reason: data[i].reason,
+          });
+        }
+        i++;
+      }
+      return khalid;
+    });
 }
