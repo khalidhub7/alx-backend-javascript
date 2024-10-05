bca48050c8b8e4f1d79b9ddcc79ba6ba10e877d0
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 19:06:40 -0700

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 import { uploadPhoto, createUser } from './utils';
+
 export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
+}
+/* export default async function asyncUploadUser() {
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     return { photo, user };
   } catch {
     return { photo: null, user: null };
   }
-}
+} */
