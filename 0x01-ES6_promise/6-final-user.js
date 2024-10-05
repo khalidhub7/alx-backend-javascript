f39f2117970e9fd416ba7185661905f66d0f12a3
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:24:39 -0700

    handle using allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -12,29 +12,3 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     })),
   ]);
 }
-/* export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ])
-    .then((data) => {
-      const khalid = [];
-      let i = 0;
-      while (i < data.length) {
-        if (data[i].status === 'fulfilled') {
-          khalid.push({
-            status: data[i].status,
-            value: data[i].value,
-          });
-        } else if (data[i].status === 'rejected') {
-          khalid.push({
-            status: data[i].status,
-            reason: data[i].reason,
-          });
-        }
-        i++;
-      }
-      return khalid;
-    });
-} */
