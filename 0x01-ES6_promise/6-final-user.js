9604679d7c9f497539f57f3e79831cc29fe1fd2c
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 14:26:39 -0700

    fix #2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,8 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+#!/usr/bin/env node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
+export default async (firstName, lastName, fileName) => {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
@@ -12,5 +13,29 @@ const handleProfileSignup = async (firstName, lastName, fileName) => {
     value: status === 'fulfilled' ? value : String(reason),
   }));
 };
-
-export default handleProfileSignup;
+/* export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ])
+    .then((data) => {
+      const khalid = [];
+      let i = 0;
+      while (i < data.length) {
+        if (data[i].status === 'fulfilled') {
+          khalid.push({
+            status: data[i].status,
+            value: data[i].value,
+          });
+        } else if (data[i].status === 'rejected') {
+          khalid.push({
+            status: data[i].status,
+            reason: data[i].reason,
+          });
+        }
+        i++;
+      }
+      return khalid;
+    });
+} */
