1aa0b26b91ed1625c439a0089ba06e84f197e189
khalidhub7 <louguikhalid7@gmail.com>
2024-10-04 17:49:08 -0700

    use allSettled

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
+#!/usr/bin/env node
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
-    .race([chinaDownload, USDownload])
-    .then((res) => res);
+    .race([chinaDownload, USDownload]);
 }
