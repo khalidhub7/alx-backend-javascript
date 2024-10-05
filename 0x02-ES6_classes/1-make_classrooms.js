d906ea5d36ac32e28700e368de812ef8a1cb90a4
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 15:15:15 -0700

    classRoom obj

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
-const initializeRooms = () => {
-  const list = [19, 20, 34];
-  list.forEach((room, index) => {
-    list[index] = new ClassRoom(room);
-  });
-  return list;
-};
-export default initializeRooms;
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+}
