a3549dd35b99a504fcef7d2c88ce9a604d775561
khalidhub7 <louguikhalid7@gmail.com>
2024-10-06 14:46:41 -0700

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
