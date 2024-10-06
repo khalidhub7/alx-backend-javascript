c7f9f08ba567b00ee5eb8f460e077d4aa5003121
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 17:06:24 -0700

    check if a method exist in sub class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor.name != 'Building') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override e\
+vacuationWarningMessage');
+      }
     }
-    this.sqft = sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._sqft = sqft;
   }
 }
