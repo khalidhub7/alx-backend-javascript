10c17c41bfd6ff2a7b11d9d229412f51aa74c4bd
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 22:30:40 -0700

    ffix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,48 +1,13 @@
-#!/usr/bin/env node
-// eslint-disable-next-line import/extensions
 import signUpUser from './4-user-promise';
-// eslint-disable-next-line import/extensions
 import uploadPhoto from './5-photo-reject';
-// eslint-disable-next-line no-unused-vars
-export default async (firstName, lastName, fileName) => {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
 
-  return results.map(({ status, value, reason }) => ({
-    status,
-    value: status === 'fulfilled' ? value : String(reason),
-  }));
-};
-
-/* 
-// eslint-disable-next-line import/extensions
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ])
-    .then((data) => {
-      const khalid = [];
-      let i = 0;
-      // eslint-disable-next-line no-plusplus
-      while (i < data.length) {
-        if (data[i].status === 'fulfilled') {
-          khalid.push({
-            status: data[i].status,
-            value: data[i].value,
-          });
-        } else if (data[i].status === 'rejected') {
-          khalid.push({
-            status: data[i].status,
-            reason: data[i].reason,
-          });
-        }
-        i++;
-      }
-      return khalid;
-    });
-} 
-*/
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
