369998050c22ba8cab32b5a82ff6b5cd903d8560
khalidhub7 <louguikhalid7@gmail.com>
2024-10-06 16:04:42 -0700

    fix ES6

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,9 @@
+#!/usr/bin/env node
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    const add = () => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    };
+    const add = () => self.sanFranciscoNeighborhoods.push(newNeighborhood);
     add();
     return self.sanFranciscoNeighborhoods;
   };
