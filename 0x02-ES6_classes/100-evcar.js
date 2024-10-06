c9e9257ed833f43911b332e699025e23b7b71898
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 21:52:43 -0700

    return obj in other not in this

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,10 @@
-import Car from './10-car';
-
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = typeof range === 'string' ? range : undefined;
+    this._range = range;
   }
 
-  // eslint-disable-next-line
   cloneCar() {
-    return new Car();
+    return new Car(Car._brand, Car._motor, Car.color, Car._range);
   }
 }
