52e6bee9efead6c7f81e97cf8e59ccef484b1570
khalidhub7 <louguikhalid7@gmail.com>
2024-10-06 16:51:07 -0700

    value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,10 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
-    income,
-    gdp,
-    capita,
-  };
-  return budget;
+#!/usr/bin/env node
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  let i = 0;
+  for (const idx of array) {
+    result[i] = appendString + idx;
+    i += 1;
+  }
+  return result;
 }
