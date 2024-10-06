df54c7627da4760719a8b5eef5f2ce63be047202
khalidhub7 <louguikhalid7@gmail.com>
2024-10-05 21:17:34 -0700

    fix hhh

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -12,8 +12,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -26,13 +24,15 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this.holbertonClass;
   }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
