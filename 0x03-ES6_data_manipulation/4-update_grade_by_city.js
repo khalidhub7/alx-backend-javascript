e86b07c2f6126ff26d8f92ebb4bd499fd2aa5385
khalidhub7 <louguikhalid7@gmail.com>
2024-10-07 16:14:34 -0700

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-  const filtered = arr.filter((x) => x.location === city);
-  const mapping = filtered.map((x) => ({
+  const filtered = arr.filter((x) => x.location === city).map((x) => ({
     id: x.id,
     firstName: x.firstName,
     location: x.location,
-    grade: newGrades.find((i) => i.studentId === x.id) ? (newGrades.find((i) => i.studentId === x.id)).grade : 'N/A',
+    grade: newGrades.find((i) => i.studentId === x.id) ? (
+      newGrades.find((i) => i.studentId === x.id)).grade : 'N/A',
   }));
-  return mapping;
+  return filtered;
 }
