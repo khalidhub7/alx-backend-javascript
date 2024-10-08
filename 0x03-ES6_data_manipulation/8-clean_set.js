3baf99adf16089686285cb921b90a785bc482437
khalidhub7 <louguikhalid7@gmail.com>
2024-10-08 16:46:26 -0700

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,14 +5,17 @@ export default function cleanSet(set, start) {
     !(set instanceof Set)
     || start.length === 0
     || typeof start !== 'string'
+    || set.size === 0
   ) {
     return '';
   }
   for (const i of set) {
-    if (i.startsWith(start)) {
-      const newstr = i.slice(start.length);
-      if (newstr.length > 0) {
-        filtred.push(newstr);
+    if (typeof (i) === 'string' && typeof(i) !== "undefined") {
+      if (i.startsWith(start)) {
+        const newstr = i.slice(start.length);
+        if (newstr.length > 0) {
+          filtred.push(newstr);
+        }
       }
     }
   }
