ed4a45df52e7ccff54a8fab0d565f0852724503d
khalidhub7 <louguikhalid7@gmail.com>
2024-10-08 17:19:54 -0700

    fix (handle other cases)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+export default function groceriesList() {
+  const keys = ['Apples', 'Tomatoes',
+    'Pasta', 'Rice', 'Banana'];
+  const values = [10, 10, 1, 1, 5];
+  const map = new Map();
+  keys.forEach((key, i) => {
+    map.set(key, values[i]);
+  });
+  return map;
+}
