fdf4e4ce5f7e03850c1728d144dd07773761f7f4
khalidhub7 <louguikhalid7@gmail.com>
2024-10-11 18:42:36 -0700

    all task 2 requirements

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,68 +1,60 @@
-export interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
 }
 
-export interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-export class Director implements DirectorInterface {
-  workFromHome() {
+class Director implements DirectorInterface {
+  workFromHome(): string {
     return 'Working from home';
   }
 
-  getCoffeeBreak() {
+  getCoffeeBreak(): string {
     return 'Getting a coffee break';
   }
 
-  workDirectorTasks() {
+  workDirectorTasks(): string {
     return 'Getting to director tasks';
   }
 }
 
-export class Teacher implements TeacherInterface {
-  workFromHome() {
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
     return 'Cannot work from home';
   }
 
-  getCoffeeBreak() {
+  getCoffeeBreak(): string {
     return 'Cannot have a break';
   }
 
-  workTeacherTasks() {
+  workTeacherTasks(): string {
     return 'Getting to work';
   }
 }
 
-export function createEmployee(salary: (number | string)): (Director | Teacher) {
-  if (typeof salary === 'number' && salary < 500) {
+function createEmployee(salary: number | string) {
+  if (typeof (salary) === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
-
-export function isDirector(employee: (Director | Teacher)) {
+function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-export function executeWork(employee: (Director | Teacher)) {
+function executeWork(employee: Director | Teacher) {
   if (isDirector(employee)) {
-    return (employee as Director).workDirectorTasks();
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
   }
-  return (employee as Teacher).workTeacherTasks();
 }
-
-export type Subjects = ('Math' | 'History');
-
-export function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  }
-  if (todayClass === 'History') {
-    return 'Teaching History';
-  }
+type Subjects = 'Math' | 'History'
+function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`;
 }
