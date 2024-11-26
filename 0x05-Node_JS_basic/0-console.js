4e6a46b443a09648694f572d6e3c82f4df738f51
khalidhub7 <louguikhalid7@gmail.com>
2024-11-25 22:25:23 -0800

    print in STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
-function displayMessage (msg) {
+#!/usr/bin/node
+function displayMessage(message) {
   process.stdout.write(msg);
   process.stdout.write('\n');
 }
+
 module.exports = displayMessage;
