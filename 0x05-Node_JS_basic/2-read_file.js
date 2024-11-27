375e90f02dc06de334c70ec9f26186ae3abd49e4
khalidhub7 <louguikhalid7@gmail.com>
2024-11-26 23:02:48 -0800

    read  a file sync node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,37 +2,41 @@ const fs = require('fs');
 
 function countStudents (path) {
   try {
-    if (path !== 'database.csv') {
-      throw new Error();
-    }
-
     const data = fs.readFileSync(path, 'utf8');
     const len = (data.split('\n').length) - 2;
     const fields = [];
 
     for (const line of data.split('\n')) {
-      const field = line.split(',')[3];
-      if (field !== undefined &&
+      if (line !== '') {
+        const field = line.split(',')[3];
+        if (field !== undefined &&
             field !== 'field' &&
             !fields.includes(field)) {
-        fields.push(field);
+          fields.push(field);
+        }
       }
     }
 
     console.log(`Number of students: ${len}`);
+
     for (const field of fields) {
-      if (field) {
+      if (field !== '') {
         const students = [];
         for (const line of data.split('\n').slice(1)) {
-          // eslint-disable-next-line no-unused-vars
-          for (const _ of line.split(',')) {
-            if (line.split(',')[3] === field &&
+          if (line !== '') {
+            // eslint-disable-next-line no-unused-vars
+            for (const _ of line.split(',')) {
+              if (line.split(',').length === 4) {
+                if (line.split(',')[3] === field &&
                         !students.includes(line.split(',')[0])
-            ) {
-              students.push(line.split(',')[0]);
+                ) {
+                  students.push(line.split(',')[0]);
+                }
+              }
             }
           }
         }
+
         console.log(`Number of students in \
 ${field}: ${students.length}. List: ${students.join(', ')}`);
       }
