d84803bcc900425c079f2ae70cb3d31972a638ba
khalidhub7 <louguikhalid7@gmail.com>
2024-11-26 18:15:30 -0800

    retrieve from stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write(
+    'Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 if (process.stdin.isTTY) {
@@ -12,7 +13,8 @@ if (process.stdin.isTTY) {
     process.stdout.write(`Your name is: ${chunk}`);
   });
   process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+    process.stdout.write(
+        'This important software is now closing\n');
     process.exit();
   });
 }
