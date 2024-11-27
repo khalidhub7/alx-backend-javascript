a19c543e42f7da3e7fa30375cee3c5e51114302c
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 02:49:45 -0800

    read  a file sync node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,41 +2,33 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8').split('\n').slice(1, -1);
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').slice(1, -1);
 
-    // handle lines
-    const lines = [];
-    for (const line of data) {
+    const validLines = lines.filter((line) => {
       const items = line.split(',');
-      if (items.length === 4 && items.every(item => item.trim())) {
-        lines.push(line);
-      }
-    }
-    console.log(`Number of students: ${lines.length}`);
+      return items.length === 4 && items.every((item) => item.trim());
+    });
 
-    // handle exists fields
-    const fields = [];
-    for (const line of lines) {
+    console.log(`Number of students: ${validLines.length}`);
+
+    const fields = validLines.reduce((acc, line) => {
       const items = line.split(',');
       const field = items[3];
-      if (!fields.includes(field)) {
-        fields.push(field);
+      if (!acc.includes(field)) {
+        acc.push(field);
       }
-    }
+      return acc;
+    }, []);
 
-    // students by field
-    for (const field of fields) {
-      const students = [];
-      for (const line of lines) {
-        const items = line.split(',');
-        if (items[3] === field) {
-          students.push(items[0]);
-        }
-      }
+    fields.forEach((field) => {
+      const students = validLines
+        .filter((line) => line.split(',')[3] === field)
+        .map((line) => line.split(',')[0]);
       console.log(
-        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
       );
-    }
+    });
   } catch (error) {
     throw new Error('Cannot load the database');
   }
