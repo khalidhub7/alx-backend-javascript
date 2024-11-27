4aed7a064532379a20d8069eb6df5a6673996a3a
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 02:56:05 -0800

    read  a file sync node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,30 +2,26 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8').split('\n').slice(1, -1);
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').slice(1, -1);
 
-    // Filter valid lines
-    const lines = data.filter((line) => {
+    const validLines = lines.filter((line) => {
       const items = line.split(',');
       return items.length === 4 && items.every((item) => item.trim());
     });
 
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${validLines.length}`);
 
-    // Extract unique fields
-    const fields = [...new Set(lines.map((line) => line.split(',')[3]))];
+    const fields = [...new Set(validLines.map((line) => line.split(',')[3]))];
 
-    // Students by field
     fields.forEach((field) => {
-      const students = lines
+      const students = validLines
         .filter((line) => line.split(',')[3] === field)
         .map((line) => line.split(',')[0]);
 
-      console.log(
-        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
-      );
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     });
-  } catch (error) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
