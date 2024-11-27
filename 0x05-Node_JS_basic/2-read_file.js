7586fefc425181ee7288448ca01ff448d04bcc1a
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 02:51:54 -0800

    read  a file sync node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,33 +2,44 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').slice(1, -1);
+    const data = fs.readFileSync(path, 'utf8').split('\n').slice(1, -1);
 
-    const validLines = lines.filter((line) => {
+    /* handle lines */
+    const lines = [];
+    for (const line of data) {
       const items = line.split(',');
-      return items.length === 4 && items.every((item) => item.trim());
-    });
+      if (
+        items.length === 4 &&
+        items.every((item) => item.trim())
+      ) {
+        lines.push(line);
+      }
+    }
+    console.log(`Number of students: ${lines.length}`);
 
-    console.log(`Number of students: ${validLines.length}`);
-
-    const fields = validLines.reduce((acc, line) => {
+    /* handle exists fields */
+    const fields = [];
+    for (const line of lines) {
       const items = line.split(',');
       const field = items[3];
-      if (!acc.includes(field)) {
-        acc.push(field);
+      if (!fields.includes(field)) {
+        fields.push(field);
       }
-      return acc;
-    }, []);
+    }
 
-    fields.forEach((field) => {
-      const students = validLines
-        .filter((line) => line.split(',')[3] === field)
-        .map((line) => line.split(',')[0]);
+    /* students by field */
+    for (const field of fields) {
+      const students = [];
+      for (const line of lines) {
+        const items = line.split(',');
+        if (items[3] === field) {
+          students.push(items[0]);
+        }
+      }
       console.log(
         `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
       );
-    });
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
