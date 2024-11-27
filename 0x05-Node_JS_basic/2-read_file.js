ec0e18173005ac08bbab95dd22de3e743b5edd20
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 02:47:25 -0800

    read  a file sync node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8').split('\n').slice(1, -1);
 
+    // handle lines
     const lines = [];
     for (const line of data) {
       const items = line.split(',');
-      if (items.length === 4 &&
-        items.every(item => item.trim())) {
+      if (items.length === 4 && items.every(item => item.trim())) {
         lines.push(line);
       }
     }
     console.log(`Number of students: ${lines.length}`);
 
-
+    // handle exists fields
     const fields = [];
     for (const line of lines) {
       const items = line.split(',');
@@ -24,7 +24,7 @@ function countStudents (path) {
       }
     }
 
-
+    // students by field
     for (const field of fields) {
       const students = [];
       for (const line of lines) {
@@ -33,7 +33,9 @@ function countStudents (path) {
           students.push(items[0]);
         }
       }
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      console.log(
+        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+      );
     }
   } catch (error) {
     throw new Error('Cannot load the database');
