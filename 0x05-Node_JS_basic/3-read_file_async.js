e6e81793913a812285e27ffd7606b0fa39c871ac
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 22:52:29 -0800

    read a file async node js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,54 @@
-#!/usr/bin/node
 const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n');
-        const students = lines
-          .filter((line) => line)
-          .map((line) => line.split(','));
-        const studentsCount = students.length - 1;
-        const studentsByField = students.slice(1).reduce((acc, student) => {
-          const field = student[3];
-          if (!acc[field]) acc[field] = [];
-          acc[field].push(student[0]);
-          return acc;
-        }, {});
-        console.log(`Number of students: ${studentsCount}`);
-        for (const field in studentsByField) {
-          if (field) {
-            const list = studentsByField[field];
-            console.log(
-              `Number of students in ${field}: ${
-                list.length
-              }. List: ${list.join(', ')}`,
-            );
-          }
+    fs.readFile(
+      path, 'utf8',
+      (err, data) => {
+        // handles I/O errors
+        if (err) {
+          return reject(new Error(
+            'Cannot load the database',
+          ));
         }
-        resolve();
-      }
-    });
+
+        try {
+          const lines = data.split('\n').slice(1).filter(
+            (line) => line.trim() !== '',
+          );
+          console.log(`Number of students: ${lines.length}`);
+
+          // handle exists fields
+          const fields = [];
+          for (const line of lines) {
+            const items = line.split(',');
+            const field = items[3];
+            if (!fields.includes(field)) {
+              fields.push(field);
+            }
+          }
+
+          // students by field
+          for (const field of fields) {
+            const students = [];
+            for (const line of lines) {
+              const items = line.split(',');
+              if (items[3] === field) {
+                students.push(items[0]);
+              }
+            }
+            console.log(`Number of students in ${field}\
+: ${students.length}. List: ${students.join(', ')}`);
+          }
+          // resolve returned when processing is done
+          return resolve();
+        } catch (error) {
+          return reject(new Error(
+            'Cannot load the database',
+          ));
+        }
+      },
+    );
   });
 }
 
