3a6bcdfd2ff791dbdd56f1b92ac55fe7f5b9ee32
khalidhub7 <louguikhalid7@gmail.com>
2024-11-27 23:55:14 -0800

    small HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,14 @@
-const { createServer } = require('http');
+const { createServer } = require('node:http');
 
+const host = 'localhost';
 const port = 1245;
 
-const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+const app = createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain')
+  response.end('Hello Holberton School!');
 });
 
-app.listen(port, 'localhost');
+app.listen((port, host));
 
 module.exports = app;
