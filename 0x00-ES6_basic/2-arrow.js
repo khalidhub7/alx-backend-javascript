5d7c2b046526a7f105c0e9a690d865cf955c2444
khalidhub7 <louguikhalid7@gmail.com>
2025-03-14 09:06:37 +0100

    arrow functions ##

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,20 @@
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+#!/usr/bin/env node
+
+function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = [
+    'SOMA', 'Union Square'];
 
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
+  /* this.addNeighborhood = (newNeighborhood) => {
+    const add = (
+    ) => this.sanFranciscoNeighborhoods.push(
+      newNeighborhood,
+    );
+    add();
+    return this.sanFranciscoNeighborhoods;
+  }; */
 }
+export default getNeighborhoodsList;
