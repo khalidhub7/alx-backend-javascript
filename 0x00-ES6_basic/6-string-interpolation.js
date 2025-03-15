196b8306896c8f2e2faf1afd5779228754c87281
khalidhub7 <louguikhalid7@gmail.com>
2025-03-15 04:29:39 +0100

    string interpolation ##

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-export default function getSanFranciscoDescription() {
+
+const getSanFranciscoDescription = () => {
   const year = 2017;
   const budget = {
     income: '$119,868',
@@ -7,9 +8,10 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest i\
-ncome county in the United States, with a per capita pe\
-rsonal income of ${budget.income}. As of 2015, San Franci\
-sco proper had a GDP of ${budget.gdp}, and a GDP per capit\
-a of ${budget.capita}.`;
-}
+  return `As of ${year}, it was the seventh-highes\
+t income county in the United States, with a per ca\
+pita personal income of ${budget.income}. As of 2015\
+, San Francisco proper had a GDP of ${budget.gdp}, an\
+d a GDP per capita of ${budget.capita}.`;
+};
+export default getSanFranciscoDescription;
