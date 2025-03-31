a15237b41ae971faed9eff3c3211b4d71fc84622
khalidhub7 <louguikhalid7@gmail.com>
2025-03-31 21:27:40 +0200

    iterate over object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-const createIteratorObject = (
-  report,
-) => {
+const createIteratorObject = (report) => {
   const obj = {};
-  // 'for...of' need 'Symbol.iterator' to iterate over obj
+
+  // 'for...of' needs 'Symbol.iterator' to iterate over obj
   obj[Symbol.iterator] = () => {
     let i = 0;
     const employees = Object.values(report.allEmployees).flat(Infinity);
+
     return {
-      // 'next' is a method of the object returned by 'Symbol.iterator',
-      // after it called by 'for...of'
+      // 'next' is called by 'for...of'
       next() {
         const value = employees[i];
         i += 1;
@@ -18,7 +17,8 @@ const createIteratorObject = (
       },
     };
   };
-  // console.log(obj);
+
   return obj;
 };
+
 export default createIteratorObject;
