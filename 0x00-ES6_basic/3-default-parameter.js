ab930c0d37b7d2c9847df5e745924932c8e1a7c6
khalidhub7 <louguikhalid7@gmail.com>
2025-03-31 23:59:45 +0200

    ## parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const getSumOfHoods = (
-  initialNumber, expansion1989 = 89,
-  expansion2019 = 19,
-) => initialNumber + expansion1989 + expansion2019;
-
+  initialNumber, expansion1989 = 89, expansion2019 = 19,
+) => (initialNumber + expansion1989 + expansion2019);
 export default getSumOfHoods;
