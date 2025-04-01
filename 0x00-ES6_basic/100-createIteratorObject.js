dece4bc6e88a18cecd1d3c8a296fb0722aa74997
khalidhub7 <louguikhalid7@gmail.com>
2025-04-01 22:45:15 +0200

    ## make object iterable using for...of

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,24 +1,18 @@
 #!/usr/bin/env node
 
-const createIteratorObject = (report) => {
-  const obj = {};
-
-  // 'for...of' needs 'Symbol.iterator' to iterate over obj
-  obj[Symbol.iterator] = () => {
+const createIteratorObject = (report) => ({
+  [Symbol.iterator]() {
+    const employees = Object.values(
+      report.allEmployees,
+    ).flat(Infinity);
     let i = 0;
-    const employees = Object.values(report.allEmployees).flat(Infinity);
-
     return {
-      // 'next' is called by 'for...of'
       next() {
         const value = employees[i];
         i += 1;
         return { value, done: i > employees.length };
       },
     };
-  };
-
-  return obj;
-};
-
+  },
+});
 export default createIteratorObject;
