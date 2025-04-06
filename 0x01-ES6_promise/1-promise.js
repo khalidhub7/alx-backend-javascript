db333178e8345d01b3750deed1297d1794def4d5
khalidhub7 <louguikhalid7@gmail.com>
2025-04-06 03:26:19 +0100

    make a promise ##

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-const getFullResponseFromAPI = async (
-  success) => (success
-  ? { status: 200, body: 'Success' }
-  : new Error('The fake API is not working currently'));
+const getFullResponseFromAPI = (
+  success,
+) => (success
+  ? Promise.resolve({ status: 200, body: 'Success' })
+  : Promise.reject(new Error(
+    'The fake API is not working currently',
+  )));
 export default getFullResponseFromAPI;
