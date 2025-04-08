c47b2a717e060a91ccb9f7df9c8652fdf289c5ec
khalidhub7 <louguikhalid7@gmail.com>
2025-04-08 22:33:55 +0100

    all ahbibi all

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,23 +1,11 @@
 #!/usr/bin/env node
+
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
-  let res = {};
-
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
-  }
-  return res;
-}
-/* export default async function asyncUploadUser() {
-  try {
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    return { photo, user };
-  } catch {
-    return { photo: null, user: null };
-  }
-} */
+const asyncUploadUser = () => (
+  Promise
+    .all([uploadPhoto(), createUser()])
+    .then((val) => ({ photo: val[0], user: val[1] }))
+    .catch(() => ({ photo: null, user: null }))
+);
+export default asyncUploadUser;
