869bf73da92dd041eb7e8bcdf2b8038b7dd3be25
khalidhub7 <louguikhalid7@gmail.com>
2025-04-08 18:32:35 +0100

    multiple promises ahbibi

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,41 +1,14 @@
 #!/usr/bin/env node
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async (firstName, lastName, fileName) => {
-  const results = await Promise.allSettled([
+const handleProfileSignup = async (
+  firstName, lastName, fileName,
+) => Promise.allSettled(
+  [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
-  return results.map(({ status, value, reason }) => ({
-    status,
-    value: status === 'fulfilled' ? value : String(reason),
-  }));
-};
-
-/* export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ])
-    .then((data) => {
-      const khalid = [];
-      let i = 0;
-      while (i < data.length) {
-        if (data[i].status === 'fulfilled') {
-          khalid.push({
-            status: data[i].status,
-            value: data[i].value,
-          });
-        } else if (data[i].status === 'rejected') {
-          khalid.push({
-            status: data[i].status,
-            reason: data[i].reason,
-          });
-        }
-        i++;
-      }
-      return khalid;
-    });
-} */
+  ],
+);
+export default handleProfileSignup;
