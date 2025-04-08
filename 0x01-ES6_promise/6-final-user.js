63bc51d1d2f3efcb5550e13a91f99b6ac5ed4216
khalidhub7 <louguikhalid7@gmail.com>
2025-04-08 19:42:58 +0100

    multiple promises ahbibi (fix .then)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,20 +3,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = (
-  firstName, lastName, fileName,
-) => Promise.allSettled(
-  [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ],
-).then((results) => {
-  for (const result of results) {
-    if ('reason' in result) {
-      result.value = `Error: ${result.reason.message}`;
-      delete result.reason;
-    }
+const handleProfileSignup = (firstName, lastName, fileName) => Promise.allSettled([
+  signUpUser(firstName, lastName),
+  uploadPhoto(fileName),
+]).then((results) => results.map((result) => {
+  if (result.status === 'rejected') {
+    return { status: result.status, value: `Error: ${result.reason.message}` };
   }
-  return results;
-});
+  return { status: result.status, value: result.value };
+}));
+
 export default handleProfileSignup;
