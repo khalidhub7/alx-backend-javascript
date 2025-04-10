0ae19fe384d562186e4ef966f1c67e5fde493908
khalidhub7 <louguikhalid7@gmail.com>
2025-04-10 03:14:03 +0100

    define class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,10 @@
-export default class ClassRoom {
+#!/usr/bin/env node
+
+class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = Number(
+      maxStudentsSize,
+    );
   }
 }
+export default ClassRoom;
