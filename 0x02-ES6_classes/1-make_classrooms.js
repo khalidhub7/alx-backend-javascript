88db980b31a7037ee4c4319782268fd340d3472b
khalidhub7 <louguikhalid7@gmail.com>
2025-04-10 03:32:15 +0100

    make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,6 @@
 
 import ClassRoom from './0-classroom';
 
-const initializeRooms = () => (
-  [19, 20, 34].map((i) => new ClassRoom(i))
-);
+const initializeRooms = () => ([19, 20, 34]
+  .map((i) => new ClassRoom(i)));
 export default initializeRooms;
