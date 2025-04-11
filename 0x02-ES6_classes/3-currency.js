214a168220d2c2e38aff04470d8ea8c43d7ef1fd
khalidhub7 <louguikhalid7@gmail.com>
2025-04-11 03:05:39 +0100

    implement Currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,45 @@
-export default class Currency {
-  constructor(code, name) {
-    this.code = code;
-    this.name = name;
+class Currency {
+  constructor(code = '', name = '') {
+    if (typeof (code) !== 'string') {
+      throw new TypeError(
+        'Code must be a string',
+      );
+    }
+    this._code = code;
+    if (typeof (name) !== 'string') {
+      throw new TypeError(
+        'Name must be a string',
+      );
+    }
+    this._name = name;
   }
 
-  set code(value) {
-    this._code = value;
+  set code(newvalue) {
+    if (typeof (newvalue) === 'string') {
+      this._code = newvalue;
+    } else {
+      throw new TypeError(
+        'Code must be a string',
+      );
+    }
   }
 
-  set name(value) {
-    this._name = value;
+  set name(newvalue) {
+    if (typeof (newvalue) === 'string') {
+      this._name = newvalue;
+    } else {
+      throw new TypeError(
+        'Name must be a string',
+      );
+    }
   }
 
-  get code() {
-    return this._code;
-  }
+  get code() { return this._code; }
 
-  get name() {
-    return this._name;
-  }
+  get name() { return this._name; }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
+export default Currency;
