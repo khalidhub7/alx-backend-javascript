915ff7c09520b7d76679a750e0961825664b9354
khalidhub7 <louguikhalid7@gmail.com>
2025-04-12 22:48:42 +0100

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,25 @@
-export default class Building {
+class Building {
   constructor(sqft) {
-    if (this.constructor.name !== 'Building') {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+    if (typeof (sqft) !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value === 'number') {
-      this._sqft = value;
+    if (new.target !== Building
+       && this.evacuationWarningMessage === undefined) {
+      throw new Error(
+        'Class extending Building must override'
+         + ' evacuationWarningMessage',
+      );
     }
   }
 
-  get sqft() {
-    return this._sqft;
+  set sqft(newvalue) {
+    if (typeof (newvalue) !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newvalue;
   }
+
+  get sqft() { return this._sqft; }
 }
+export default Building;
