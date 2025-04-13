5783a99562f4bdf15b4dbe1606dbfc7d78af8b0e
khalidhub7 <louguikhalid7@gmail.com>
2025-04-13 01:18:23 +0100

    basic inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,41 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
-  }
-
-  set floors(value) {
-    if ((typeof value) === 'number') {
-      this._floors = value;
+    if (typeof (floors) !== 'number') {
+      throw new TypeError(
+        'floors must be a number',
+      );
     }
+    this._floors = floors;
   }
 
-  get floors() {
-    return this._floors;
+  set sqft(newvalue) {
+    if (typeof (newvalue) !== 'number') {
+      throw new TypeError(
+        'sqft must be a number',
+      );
+    }
+    this._sqft = newvalue;
   }
+
+  set floors(newvalue) {
+    if (typeof (newvalue) !== 'number') {
+      throw new TypeError(
+        'floors must be a number',
+      );
+    }
+    this._floors = newvalue;
+  }
+
+  get sqft() { return this._sqft; }
+
+  get floors() { return this._floors; }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the \
+${this._floors} floors`;
   }
 }
+export default SkyHighBuilding;
