3810181553aa9dc512ecabc03f8af931e5daef65
khalidhub7 <louguikhalid7@gmail.com>
2025-04-13 20:32:11 +0100

    add custom Tag

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,18 @@
-export default class Airport {
+class Airport {
   constructor(name, code) {
+    [name, code].forEach((i) => {
+      if (typeof i !== 'string') {
+        throw new TypeError(
+          `${i} must be a string`,
+        );
+      }
+    });
     this._name = name;
     this._code = code;
   }
 
-  toString() {
-    return `[${typeof (this)} ${this._code}]`;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 }
+export default Airport;
