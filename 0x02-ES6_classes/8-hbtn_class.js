bb2851af46e28e3e7a04d765c576cc48acd0f1ee
khalidhub7 <louguikhalid7@gmail.com>
2025-04-16 00:26:08 +0100

    to primitive

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,14 +1,19 @@
-export default class HolbertonClass {
+class ALXClass {
   constructor(size, location) {
+    if (typeof size !== 'number'
+    ) throw new TypeError('size must be a number');
+    if (typeof location !== 'string'
+    ) throw new TypeError('location must be a string');
     this._size = size;
     this._location = location;
   }
 
-  toString() {
-    return String(this._location);
-  }
-
-  valueOf() {
-    return Number(this._size);
+  [Symbol.toPrimitive](type) {
+    if (type === 'number'
+    ) { return this._size; }
+    if (type === 'string'
+    ) { return this._location; }
+    return this.toString();
   }
 }
+export default ALXClass;
