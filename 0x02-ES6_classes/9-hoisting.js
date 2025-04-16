2970f3147540b34d3e638d0891f3054accdda155
khalidhub7 <louguikhalid7@gmail.com>
2025-04-16 04:26:49 +0100

    fix errs

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -9,14 +9,11 @@ class ALXClass {
   get location() { return this._location; }
 }
 
-
 const class2019 = new ALXClass(2019, 'San Francisco');
 const class2020 = new ALXClass(2020, 'San Francisco');
 
-
-class StudentALX extends ALXClass {
+class StudentALX {
   constructor(firstName, lastName, alxClass) {
-    super(alxClass.year, alxClass.location);
     this._firstName = firstName;
     this._lastName = lastName;
     this._alxClass = alxClass;
@@ -34,7 +31,6 @@ class StudentALX extends ALXClass {
   }
 }
 
-
 const listOfStudents = [
   new StudentALX('Guillaume', 'Salva', class2020),
   new StudentALX('John', 'Doe', class2020),
@@ -42,6 +38,5 @@ const listOfStudents = [
   new StudentALX('Donald', 'Bush', class2019),
   new StudentALX('Jason', 'Sandler', class2019),
 ];
-
 
 export default listOfStudents;
