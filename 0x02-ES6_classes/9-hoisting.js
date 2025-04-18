897b6d1214c220940f615e04648014d82d99474b
khalidhub7 <louguikhalid7@gmail.com>
2025-04-18 04:47:25 +0100

    fix errs ##

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -28,12 +28,8 @@ class StudentALX {
   }
 }
 
-// lets change names for testing purpose
-const HolbertonClass = class extends ALXClass {};
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
+// let's adapt class names to match test requirements
+class HolbertonClass extends ALXClass {}
 class StudentHolberton extends StudentALX {
   constructor(firstName, lastName, alxClass) {
     super(firstName, lastName, alxClass);
@@ -43,6 +39,9 @@ class StudentHolberton extends StudentALX {
 
   get alxClass() { return this.holbertonClass; }
 }
+// let's create test instances
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 const listOfStudents = [
   new StudentHolberton('Guillaume', 'Salva', class2020),
   new StudentHolberton('John', 'Doe', class2020),
