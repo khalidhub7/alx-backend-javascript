feb53883aaf01f85f47f6cda4023e25627a9d769
khalidhub7 <louguikhalid7@gmail.com>
2025-04-18 05:12:43 +0100

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,18 @@
-export default class Car {
+class Car {
   constructor(brand, motor, color) {
+    [brand, motor, color].forEach((i) => {
+      if (typeof (i) !== 'string'
+      ) { throw new TypeError(`${i} must be a string`); }
+    });
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new (this.constructor)(
+      this._brand, this._motor, this._color,
+    );
   }
 }
+export default Car;
