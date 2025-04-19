92b888da7e96139725f8d8f8022e98e8b4238122
khalidhub7 <louguikhalid7@gmail.com>
2025-04-19 07:22:00 +0100

    init

---
+++ 0x00-ES6_basic/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# detect ubuntu version
+VERSION_CODENAME=$(lsb_release -c | awk '{print $2}')
+
+if [[ "$VERSION_CODENAME" == "noble" ]]; then
+	echo "Since you're using Ubuntu 24.04 (Noble), 
+we will install Node.js 18.x"
+	curl -fsSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+else
+	echo "Since you're using a different Ubuntu version, 
+we will install Node.js 12.x"
+	curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+fi
+
+# install Node.js silently
+sudo -s bash nodesource_setup.sh >/dev/null 2>&1
+sudo apt install nodejs -y >/dev/null 2>&1
+node -v >/dev/null 2>&1 && npm -v >/dev/null 2>&1
+
+# install dependencies silently
+npm install >/dev/null 2>&1
+
+: '
+make sure these files in your project directory:
+    package.json, babel.config.js, .eslintrc.js
+'
+echo "
+done"
