4d2a0f9e5b6cbadf24e00fbf7cdcc3e2115b8fc8
khalidhub7 <louguikhalid7@gmail.com>
2025-04-26 06:36:15 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,10 @@
-#!/usr/bin/env node
-export default function cleanSet(set, start) {
-  const filtered = [];
-  if (
-    !(set instanceof Set)
-      || typeof start !== 'string'
-      || start.trim().length === 0
-      || set.size === 0
-  ) {
-    return '';
-  }
-  for (const i of set) {
-    if (typeof i === 'string' && i
-         !== null && i.startsWith(start)) {
-      const newstr = i.slice(start.length);
-      filtered.push(newstr);
-    }
-  }
-  return filtered.join('-');
-}
+const cleanSet = (set, startString) => (
+  startString ? Array.from(set)
+    .map((i) => (i.startsWith(startString)
+      ? i.slice(startString.length)
+      : undefined
+    ))
+    .filter((i) => i !== undefined)
+    .join('-') : ''
+);
+export default cleanSet;
