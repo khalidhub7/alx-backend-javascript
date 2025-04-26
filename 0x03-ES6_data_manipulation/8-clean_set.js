25b87c45ba1065ecb16f6fdce0681fe8a5109e64
khalidhub7 <louguikhalid7@gmail.com>
2025-04-26 06:55:01 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-nested-ternary */
 const cleanSet = (set, startString) => (
-  startString ? Array.from(set)
-    .map((i) => (i.startsWith(startString)
-      ? i.slice(startString.length)
-      : undefined
-    ))
-    .filter((i) => i !== undefined)
-    .join('-') : ''
+  typeof (startString) === 'string'
+    ? Array.from(set)
+      .map((i) => (i ? (i.startsWith(startString)
+        ? i.slice(startString.length)
+        : undefined
+      ) : undefined))
+      .filter((i) => i !== undefined)
+      .join('-') : ''
 );
 export default cleanSet;
