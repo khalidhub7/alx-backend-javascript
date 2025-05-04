48df2259f7b5c41466e9c7fa6236f76b92ee64cb
khalidhub7 <louguikhalid7@gmail.com>
2025-05-04 07:19:24 +0100

    init

---
+++ 0x04-TypeScript/task_0/setup.sh
@@ -0,0 +1,12 @@
+npm install --save-dev prettier    # install prettier
+npx prettier --write js/main.ts    # format typescript file
+
+npx eslint js/main.ts --fix        # run eslint and fix issues
+
+tsc js/main.ts && node js/main.js   # compile and run typescript
+
+
+npm install          # install dependencies
+npm run start-dev    # start dev server
+npm run build        # build project
+npm test             # run tests
