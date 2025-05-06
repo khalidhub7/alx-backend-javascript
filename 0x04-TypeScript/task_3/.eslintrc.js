f2883c5e9f1569b0f1694de997931dcdfb1add47
khalidhub7 <louguikhalid7@gmail.com>
2025-05-06 08:49:38 +0100

    initial config files

---
+++ 0x04-TypeScript/task_3/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {},
+};
