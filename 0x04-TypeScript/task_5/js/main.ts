14a31db3351167b0d7e4e697b186067ae7d5c875
khalidhub7 <louguikhalid7@gmail.com>
2025-05-14 20:24:29 +0100

    brand concept

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,15 +1,31 @@
-export interface MajorCredits {
-  credits: number & { __brand: 'MajorCredits.credits' };
+type majorCrs = number & { __brand: "majorCrs" };
+type minorCrs = number & { __brand: "minorCrs" };
+
+interface MajorCredits {
+  credits: majorCrs;
+}
+interface MinorCredits {
+  credits: minorCrs;
 }
 
-export interface MinorCredits {
-  credits: number & { __brand: 'MinorCredits.credits' };
-}
+const sumMajorCredits = (
+  subject1: MajorCredits,
+  subject2: MajorCredits,
+): MajorCredits => ({
+  credits: (subject1.credits + subject2.credits) as majorCrs,
+});
 
-export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits;
-}
+const sumMinorCredits = (
+  subject1: MinorCredits,
+  subject2: MinorCredits,
+): MinorCredits => ({
+  credits: (subject1.credits + subject2.credits) as minorCrs,
+});
 
-export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits;
-}
+/* // test
+const major1: MajorCredits = { credits: 10 as majorCrs };
+const major2: MajorCredits = { credits: 20 as majorCrs };
+console.log(sumMajorCredits(major1, major2));
+const minor1: MinorCredits = { credits: 5 as minorCrs };
+const minor2: MinorCredits = { credits: 15 as minorCrs };
+console.log(sumMinorCredits(minor1, minor2)); */
