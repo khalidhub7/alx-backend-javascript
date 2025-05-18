81b7d33ab113451d8cdfd9e18a549368ddf9c0bd
khalidhub7 <louguikhalid7@gmail.com>
2025-05-18 19:46:20 +0200

    node process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,19 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
+process.stdout.write(
+  'Welcome to ALX, what is your name?\n',
+);
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (chunk) => {
-    process.stdout.write(`Your name is: ${chunk}`);
-    process.stdin.pause();
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (chunk) => {
-    process.stdout.write(`Your name is: ${chunk}`);
-  });
-  process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  });
-}
+const istty = process.stdin.isTTY;
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(
+    `Your name is: ${data}`,
+  );
+  if (istty) {
+    process.exit(0);
+  } else {
+    process.stdout.write(
+      'This important software is now closing\n',
+    );
+    process.exit(0);
+  }
+});
