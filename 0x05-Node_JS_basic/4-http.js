ae450654a474e437b034eca5ad96f462484f075c
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 20:11:03 +0200

    basic node server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,6 +2,9 @@ const { createServer } = require('node:http');
 
 const app = createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader(
+    'Content-Type', 'text/plain',
+  );
   res.end('Hello ALX!');
 }).listen(1245, 'localhost');
 module.exports = app;
