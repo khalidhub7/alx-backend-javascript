68ec5c03631e61cbb34c07b890c1232d366d5515
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 10:23:55 +0200

    fix \r\n to give just \r

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
 process.stdout.write(
-  'Welcome to ALX, what is your name?\n',
+  'Welcome to Holberton School, what is your name?\n',
 );
 
 process.stdin
   .on('data', (data) => {
     process.stdout.write(
-      `Your name is: ${data.toString().trim()}\n`);
+      `Your name is: ${data}`,
+    );
     process.stdin.pause();
   })
   .on('end', () => {
