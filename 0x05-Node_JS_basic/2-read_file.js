c62a3d002db7f6a8c21ff7316ba1477a1c0a4cff
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 12:05:51 +0200

    read a file sync node js fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,46 +1,31 @@
-const fs = require('fs');
+const reader = require('fs');
 
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(
-      path, 'utf8',
-    ).split('\n').slice(1, -1);
+const countStudents = (path) => {
+  if (path) {
+    const content = reader.readFileSync(path, 'utf-8');
 
     // handle lines
-    const lines = [];
-    for (const line of data) {
-      const items = line.split(',');
-      if (items.length === 4
-        && items.every((item) => item.trim())) {
-        lines.push(line);
-      }
-    }
-    console.log(`Number of students: ${lines.length}`);
+    const lines = content.split('\n').slice(1)
+      .map((line) => (line.split(',')))
+      .filter(((line) => (line.length === 4
+        && line.every((i) => i !== ''))));
 
     // handle exists fields
-    const fields = [];
-    for (const line of lines) {
-      const items = line.split(',');
-      const field = items[3];
-      if (!fields.includes(field)) {
-        fields.push(field);
-      }
-    }
+    const fields = [
+      ...new Set(lines.map((line) => line[3])),
+    ];
 
-    // students by field
-    for (const field of fields) {
-      const students = [];
-      for (const line of lines) {
-        const items = line.split(',');
-        if (items[3] === field) {
-          students.push(items[0]);
-        }
-      }
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-    }
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
+    // log
+    console.log(`Number of students: ${lines.length}`);
+    fields.forEach((field) => {
+      // students by field
+      const students = lines
+        .filter((line) => (line[3] === field))
+        .map((line) => line[0]);
 
+      console.log(`Number of students in ${field}\
+: ${students.length}. List: ${students.join(', ')}`);
+    });
+  } else { throw new Error('Cannot load the database'); }
+};
 module.exports = countStudents;
