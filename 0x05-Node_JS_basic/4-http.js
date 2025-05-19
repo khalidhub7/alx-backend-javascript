970d696408d6492b40bbabc9d2ef14efe12e62bc
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 20:08:21 +0200

    basic node server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,6 +2,6 @@ const { createServer } = require('node:http');
 
 const app = createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello Holberton School!');
+  res.end('Hello ALX!');
 }).listen(1245, 'localhost');
 module.exports = app;
