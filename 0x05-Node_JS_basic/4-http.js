3dd50475cc85c13ec8518d83431c71ee25b7d5fb
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 20:12:53 +0200

    basic node server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader(
     'Content-Type', 'text/plain',
