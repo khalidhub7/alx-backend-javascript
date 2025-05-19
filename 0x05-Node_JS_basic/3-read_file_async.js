cdf8c47e780462832da41064d97a034769e44c35
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 18:46:52 +0200

    read a file async node js fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,43 +1,35 @@
 const reader = require('fs');
 
-const countStudents = (path) => new Promise(
-  (resolve, reject) => {
-    reader.readFile(path, 'utf-8', (err, data) => {
-      // console.log(`*** ${data} ***`)
+const countStudents = async (path) => {
+  try {
+    const content = await reader.readFileSync(path, 'utf-8');
+    // console.log(`*** ${content} ***`);
 
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      // handle lines
-      const lines = data.split('\n').slice(1)
-        .map((line) => (line.split(',')))
-        .filter(((line) => (line.length === 4
+    // handle lines
+    const lines = content.split('\n').slice(1)
+      .map((line) => (line.split(',')))
+      .filter(((line) => (line.length === 4
         && line.every((i) => i !== ''))));
 
-      // handle exists fields
-      const fields = [
-        ...new Set(lines.map((line) => line[3])),
-      ];
+    // handle exists fields
+    const fields = [
+      ...new Set(lines.map((line) => line[3])),
+    ];
 
-      // log
-      console.log(
-        `Number of students: ${lines.length}`,
-      );
+    // log
+    console.log(
+      `Number of students: ${lines.length}`,
+    );
 
-      fields.forEach((field) => {
-        // students by field
-        const students = lines
-          .filter((line) => (line[3] === field))
-          .map((line) => line[0]);
+    fields.forEach((field) => {
+      // students by field
+      const students = lines
+        .filter((line) => (line[3] === field))
+        .map((line) => line[0]);
 
-        console.log(`Number of students in ${field}\
+      console.log(`Number of students in ${field}\
 : ${students.length}. List: ${students.join(', ')}`);
-      });
-
-      resolve(); // resolve if everything pass good
     });
-  },
-);
+  } catch (err) { throw new Error('Cannot load the database'); }
+};
 module.exports = countStudents;
