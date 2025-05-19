312750edd9c3e203f9526c9cfef20a666c84f8bd
khalidhub7 <louguikhalid7@gmail.com>
2025-05-19 07:54:59 +0200

    node process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,18 +2,13 @@ process.stdout.write(
   'Welcome to ALX, what is your name?\n',
 );
 
-const istty = process.stdin.isTTY;
-
-process.stdin.on('data', (data) => {
-  process.stdout.write(
-    `Your name is: ${data}`,
-  );
-  if (istty) {
-    process.exit(0);
-  } else {
+process.stdin
+  .on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.stdin.pause();
+  })
+  .on('end', () => {
     process.stdout.write(
       'This important software is now closing\n',
     );
-    process.exit(0);
-  }
-});
+  });
