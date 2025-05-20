44cbcdee03b0b1296548b7fe8651326de6a4b13a
khalidhub7 <louguikhalid7@gmail.com>
2025-05-20 17:13:51 +0200

    complex express fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,48 @@
 const express = require('express');
-const { countStudents } = require('./5-http');
+// const { countStudents } = require('./5-http');
+
+// edited countStudents to return result instead of log
+const reader = require('fs');
+
+const countStudents = async (path) => {
+  try {
+    const content = await reader.promises.readFile(
+      path, 'utf-8',
+    );
+    // console.log(`*** ${content} ***`);
+
+    // handle lines
+    const lines = content
+      .split('\n').slice(1)
+      .map((line) => (line.split(',')))
+      .filter(((line) => (
+        line.length === 4 && line.every((i) => i !== '')
+      )));
+
+    // handle exists fields
+    const fields = [
+      ...new Set(lines.map((line) => line[3])),
+    ];
+
+    const logs = [
+      'This is the list of our students',
+      `Number of students: ${lines.length}`,
+    ];
+
+    fields.forEach((field) => {
+      // students by field
+      const students = lines
+        .filter((line) => (line[3] === field))
+        .map((line) => line[0]);
+
+      logs.push(
+        `Number of students in ${field}: ${students.length}. \
+List: ${students.join(', ')}`,
+      );
+    });
+    return logs.join('\n');
+  } catch (err) { throw new Error('Cannot load the database'); }
+};
 
 const app = express();
 
