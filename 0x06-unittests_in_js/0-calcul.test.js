80613a8c68d9178fc30ad4cfe9891fbf886469cb
khalidhub7 <louguikhalid7@gmail.com>
2025-05-24 22:55:56 +0200

    basic mocha & node assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,21 +3,21 @@ const { describe, it } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('test add func', () => {
-  it('test suite for first number rounded',
+  /* it('test suite for first number rounded',
     () => {
       assert.strictEqual(
         calculateNumber(1.1, 0), 1);
     });
 
-  /* it('test suite for second number rounded',
+  it('test suite for second number rounded',
     () => {
       assert.strictEqual(
         calculateNumber(0, 2.9), 3);
     }); */
 
-  /* it('test suite for both number rounded',
+  it('test suite for both number rounded',
     () => {
       assert.strictEqual(
         calculateNumber(4.9, 2.1), 7);
-    }); */
+    });
 });
