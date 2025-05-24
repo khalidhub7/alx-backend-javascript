455bd68cb9e01a5838afccd59b071e5eaecdd08b
khalidhub7 <louguikhalid7@gmail.com>
2025-05-24 21:49:43 +0200

    basic test with mocha and node assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,8 +3,18 @@ const { describe, it } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('test add func', () => {
-  it('should return 7', () => {
-    assert.strictEqual(
-      calculateNumber(8, -1), 7);
-  });
+  it('test suite for first number rounded',
+    () => {
+      assert.strictEqual(calculateNumber(1.1, 0), 1);
+    });
+
+  it('test suite for second number rounded',
+    () => {
+      assert.strictEqual(calculateNumber(0, 2.9), 3);
+    });
+
+  it('test suite for both number rounded',
+    () => {
+      assert.strictEqual(calculateNumber(4.9, 2.1), 7);
+    });
 });
