55d8a301a6ca8d7a28c3d0f592ed2024576dd321
khalidhub7 <louguikhalid7@gmail.com>
2025-05-24 19:43:37 +0200

    basic test with mocha and node assert

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,3 @@
-const calculateNumber = (a, b) => (
-  a + b
-);
-export default calculateNumber;
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
