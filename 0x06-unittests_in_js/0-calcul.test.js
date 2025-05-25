966e5b91444c9179fa8d4ed5f2b010047061b930
khalidhub7 <louguikhalid7@gmail.com>
2025-05-25 02:15:00 +0200

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,12 +5,13 @@ const calculateNumber = require('./0-calcul');
 describe(
   'test addition with 2 rounded numbers',
   () => {
-    it('rounds first number only', () => {
+    /* it('rounds first number only', () => {
       assert.strictEqual(calculateNumber(1.1, 0), 1);
-    });
-
-    /* it('rounds both numbers', () => {
-      assert.strictEqual(calculateNumber(4.9, 2.1), 7);
     }); */
+
+    it('rounds both numbers', () => {
+      assert.strictEqual(calculateNumber(4.9, 2.1), 7);
+    });
   },
 );
+
