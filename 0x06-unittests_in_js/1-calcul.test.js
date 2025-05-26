4a2e2a596b23e224e217334c241eeb40fc0aea9d
khalidhub7 <louguikhalid7@gmail.com>
2025-05-26 03:44:06 +0200

    combining descriptions##

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,26 +5,26 @@ const calculateNumber = require('./1-calcul');
 describe('1-calcul.js tests', () => {
   describe('test addition with 2 rounded numbers',
     () => {
-      /* it('rounds both numbers', () => assert.strictEqual(
+      it('rounds both numbers', () => assert.strictEqual(
         calculateNumber('SUM', 4.9, 2.1), 7,
-      )); */
+      ));
     });
 
   describe(
     'test subtraction with 2 rounded numbers',
     () => {
-      /* it('rounds both numbers', () => {
+      it('rounds both numbers', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', 4.9, 2.1), 3);
-      }); */
+      });
     },
   );
 
   describe(
     'test division with 2 rounded numbers',
     () => {
-      it('rounds first number only', () => {
+      /* it('rounds first number only', () => {
         assert.strictEqual(calculateNumber('DIVIDE', 1.1, 0), 'Error');
-      });
+      }); */
 
       it('rounds both numbers', () => {
         assert.strictEqual(calculateNumber('DIVIDE', 4.9, 2.1), 2.5);
