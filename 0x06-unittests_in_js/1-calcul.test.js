0aaf76daaf532ac40ab9791774c67b15557a1c56
khalidhub7 <louguikhalid7@gmail.com>
2025-05-26 03:41:12 +0200

    combining descriptions##

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,21 +3,19 @@ const { describe, it } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
 describe('1-calcul.js tests', () => {
-  describe(
-    'test addition with 2 rounded numbers',
+  describe('test addition with 2 rounded numbers',
     () => {
-      it('rounds both numbers', () => {
-        assert.strictEqual(calculateNumber('SUM', 4.9, 2.1), 7);
-      });
-    },
-  );
+      it('rounds both numbers', () => assert.strictEqual(
+        calculateNumber('SUM', 4.9, 2.1), 7,
+      ));
+    });
 
   describe(
     'test subtraction with 2 rounded numbers',
     () => {
-      it('rounds first number only', () => {
+      /* it('rounds first number only', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', 1.1, 0), 1);
-      });
+      }); */
 
       it('rounds both numbers', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', 4.9, 2.1), 3);
