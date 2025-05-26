a2d7cdd117228d86e2284c96217c09b5c2c3de77
khalidhub7 <louguikhalid7@gmail.com>
2025-05-27 01:35:13 +0200

    init files

--- 0x06-unittests_in_js/.eslintrc.js
+++ 0x06-unittests_in_js/.eslintrc.js
@@ -1,37 +1,26 @@
 module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-    mocha: true,
-  },
-  extends: ["airbnb-base", "plugin:jest/all", "plugin:mocha/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["jest", "mocha"],
-  settings: {
-    jest: {
-      version: "detect",
-    },
-  },
+  env: { es6: true, mocha: true },
+
+  extends: ["airbnb-base", "plugin:mocha/recommended"],
+
+  parserOptions: { ecmaVersion: 2018, sourceType: "module" },
+
+  plugins: ["mocha"],
+
   rules: {
     "max-classes-per-file": "off",
     "no-underscore-dangle": "off",
     "no-console": "off",
     "no-shadow": "off",
     "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+
     "jest/require-hook": "off",
+    "function-paren-newline": ["error", "consistent"],
+    "mocha/no-mocha-arrows": "off",
+    "no-self-compare": "off",
+    "no-unused-expressions": "off",
+    "mocha/no-skipped-tests": "off",
   },
-  overrides: [
-    {
-      files: ["*.js"],
-      excludedFiles: "babel.config.js",
-    },
-  ],
+
+  overrides: [{ files: ["*.js"], excludedFiles: ".eslintrc.js" }],
 };
