9685e4e3eb71e6d5e4c18f71a975817461643e10
khalidhub7 <louguikhalid7@gmail.com>
2025-05-26 03:52:51 +0200

    add Chai assertions to prev task ...

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,39 +6,31 @@ describe('2-calcul.js tests', () => {
   describe(
     'test addition with 2 rounded numbers',
     () => {
-      it('rounds first number only', () => {
-        expect(calculateNumber('SUM', 1.1, 0)).to.equal(1);
-      });
-
-      it('rounds both numbers', () => {
-        expect(calculateNumber('SUM', 4.9, 2.1)).to.equal(7);
-      });
+      it('rounds both numbers', () => expect(
+        calculateNumber('SUM', 4.9, 2.1)).to.equal(7)
+      );
     },
   );
 
   describe(
     'test subtraction with 2 rounded numbers',
     () => {
-      it('rounds first number only', () => {
-        expect(calculateNumber('SUBTRACT', 1.1, 0)).to.equal(1);
-      });
-
-      it('rounds both numbers', () => {
-        expect(calculateNumber('SUBTRACT', 4.9, 2.1)).to.equal(3);
-      });
+      it('rounds both numbers', () => expect(
+        calculateNumber('SUBTRACT', 4.9, 2.1)).to.equal(3)
+      );
     },
   );
 
   describe(
     'test division with 2 rounded numbers',
     () => {
-      it('rounds first number only', () => {
-        expect(calculateNumber('DIVIDE', 1.1, 0)).to.equal('Error');
-      });
+      it('rounds first number only', () => expect(
+        calculateNumber('DIVIDE', 1.1, 0)).to.equal('Error')
+      );
 
-      it('rounds both numbers', () => {
-        expect(calculateNumber('DIVIDE', 4.9, 2.1)).to.equal(2.5);
-      });
+      it('rounds both numbers', () => expect(
+        calculateNumber('DIVIDE', 4.9, 2.1)).to.equal(2.5)
+      );
     },
   );
 });
