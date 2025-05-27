d072dd4602d68a83c16489a4d1f9525c658eed52
khalidhub7 <louguikhalid7@gmail.com>
2025-05-27 17:27:16 +0200

    basic integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,13 +1,13 @@
 const express = require('express');
+
 const app = express();
-const port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200)
+    .send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
-
-module.exports = app;
+app.listen(7865, 'localhost', () => console.log(
+  'API available on localhost port 7865',
+));
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,22 +1,29 @@
-#!/usr/bin/node
-const request = require('request');
+const { describe, it } = require('mocha');
 const { expect } = require('chai');
+const request = require('request');
 
-describe('GET /', () => {
-  const ops = {
-    url: 'http://localhost:7865',
-    method: 'GET',
-  };
-  it('should return 200', (done) => {
-    request(ops, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
+describe('8-api tests', () => {
+  let statusCode;
+  let content;
+
+  before((done) => {
+    request.get('http://localhost:7865/',
+      (err, res, body) => {
+        if (err) { return done(err); }
+        statusCode = res.statusCode; content = body;
+        return done();
+      });
   });
-  it('should return Welcome to the payment system', (done) => {
-    request(ops, (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+
+  it('should return 200 status',
+    () => {
+      expect(statusCode).to.equal(200);
     });
-  });
+
+  it('should return welcome message',
+    () => {
+      expect(content).to.equal(
+        'Welcome to the payment system',
+      );
+    });
 });
