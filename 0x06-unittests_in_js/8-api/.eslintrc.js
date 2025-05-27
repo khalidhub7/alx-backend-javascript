5d79d65fb6dbde86d61f1d7f85455c2648e197db
khalidhub7 <louguikhalid7@gmail.com>
2025-05-27 17:31:48 +0200

    basic integration testing

---
+++ 0x06-unittests_in_js/8-api/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true, // stop ESLint from looking in parent folders
+  env: { es6: true, mocha: true },
+
+  extends: ["airbnb-base", "plugin:mocha/recommended"],
+
+  parserOptions: { ecmaVersion: 2018, sourceType: "module" },
+
+  plugins: ["mocha"],
+
+  rules: {
+    "max-classes-per-file": "off",
+    "no-underscore-dangle": "off",
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+
+    "jest/require-hook": "off",
+    "function-paren-newline": ["error", "consistent"],
+    "mocha/no-mocha-arrows": "off",
+    "no-self-compare": "off",
+    "no-unused-expressions": "off",
+    "mocha/no-skipped-tests": "off",
+  },
+
+  overrides: [{ files: ["*.js"], excludedFiles: ".eslintrc.js" }],
+};
